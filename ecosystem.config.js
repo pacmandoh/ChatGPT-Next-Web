d85f81a3274e090b07e3795caa1424f9b79470d8
pacmandoh <pacmandoh@outlook.com>
2023-12-12 17:51:01 +0800

    update: converted to an ES module

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,14 @@
-module.exports = {
-    apps: [
-      {
-        name: 'chatgpt',
-        script: 'node_modules/.bin/next',
-        args: 'start --port 7890',
-        instances: 'max',
-        exec_mode: 'cluster',
-        watch: false,
-        merge_logs: true,
-        env: {
-          NODE_ENV: 'production',
-        },
-      },
-    ],
-};
+export const apps = [
+  {
+    name: 'chatgpt',
+    script: 'node_modules/.bin/next',
+    args: 'start --port 7890',
+    instances: 'max',
+    exec_mode: 'cluster',
+    watch: false,
+    merge_logs: true,
+    env: {
+      NODE_ENV: 'production',
+    },
+  },
+];
