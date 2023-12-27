598899e348cbb8491f4173012773b3c089107bd2
pacmandoh <pacmandoh@outlook.com>
2023-12-27 23:04:03 +0800

    chore: adjust some details

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "PacGPTs: AI for PacDocs",
+  title: "PacGPTs - ChatAI for PacDocs",
   description: "PacDocs's AI Chat Bot.",
   viewport: {
     width: "device-width",
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#1b262a" },
   ],
   appleWebApp: {
-    title: "PacGPTs: AI for PacDocs",
+    title: "PacGPTs - AI for PacDocs",
     statusBarStyle: "default",
   },
 };
